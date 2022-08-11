4dd0df685d48a7af37d18755552462ed975e8a65
Ana <9025015386@gmail.ru>
2022-08-11 23:48:57 +0500

    2

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+// Задание 1
+        int a = 0;
+        while(a < 10) {
+            ++a;
+            System.out.print("" + a + " ");
+        }
+        System.out.println(" ");
+        int b = 11;
+        while(b > 1) {
+            --b;
+            System.out.print("" + b + " ");
+        }
+// Задание 2
+
+    }
+}
